b37e6d0dd5b4bf94f6bc65b60b7c2410042c1603
Thái <thaiqp124@gmail.com>
2026-04-14 19:48:10 +0700

    delete: tab manage vehicle, history log

--- components/image-capture-area.tsx
+++ components/image-capture-area.tsx
@@ -13,10 +13,16 @@ export function ImageCaptureArea({
   imagePlate, 
   faceImg 
 }: ImageCaptureAreaProps) {
+  const formatBase64 = (str?: string | null) => {
+    if (!str) return null;
+    if (str.startsWith('data:image')) return str; // Nếu có header rồi thì thôi
+    return `data:image/jpeg;base64,${str}`;      // Nếu chưa có thì thêm vào
+  };
+
   const images = [
-    { id: 'face', label: 'HÌNH LÁI XE', src: faceImg},
-    { id: 'front', label: 'HÌNH PHÍA TRƯỚC', src: imageFront},
-    { id: 'plate', label: 'BIỂN SỐ', src: imagePlate},
+    { id: 'face', label: 'HÌNH LÁI XE', src: formatBase64(faceImg) },
+    { id: 'front', label: 'HÌNH PHÍA TRƯỚC', src: formatBase64(imageFront) },
+    { id: 'plate', label: 'BIỂN SỐ', src: formatBase64(imagePlate) },
   ]
 
   return (
@@ -30,7 +36,7 @@ export function ImageCaptureArea({
             <img 
               src={image.src} 
               alt={image.label}
-              className="w-full h-full object-cover"
+              className="w-full h-full object-contain p-1"
             />
           ) : (
             <div className="flex flex-col items-center gap-1 text-muted-foreground">
