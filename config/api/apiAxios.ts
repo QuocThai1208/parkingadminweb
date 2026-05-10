26b1ec38cbb416c7012c1220a817070d34d7e003
Thái <thaiqp124@gmail.com>
2026-05-10 22:24:32 +0700

    update api

--- config/api/apiAxios.ts
+++ config/api/apiAxios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, AxiosResponse, AxiosError } from "axios";
 
 // 1. Khởi tạo instance
 const apiAxios: AxiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://160.191.50.68:8000/api",
   timeout: 10000,
   headers: {
     "ngrok-skip-browser-warning": "true",
